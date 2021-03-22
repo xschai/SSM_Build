4ff95042f3831f53abd1492a922b99f5aee40184
chaixs <1113935182@qq.com>
2021-03-22 16:50:32 +0800

    ssm整合

---
+++ src/main/java/com/kuang/service/BookServiceImpl.java
@@ -0,0 +1,41 @@
+package com.kuang.service;
+
+import com.kuang.dao.BookMapper;
+import com.kuang.pojo.Books;
+
+import java.util.List;
+
+public class BookServiceImpl implements BookService{
+
+    //调用dao层的操作，设置一个set接口，方便Spring管理
+    private BookMapper bookMapper;
+
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    @Override
+    public int addBook(Books book) {
+        return bookMapper.addBook(book);
+    }
+
+    @Override
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    @Override
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    @Override
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    @Override
+    public List<Books> queryAllBook() {
+        return bookMapper.queryAllBook();
+    }
+}
